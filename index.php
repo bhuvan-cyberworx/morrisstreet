c8ca705f0bfdec26255fcd355ea8af5fc089bdd0
bhuvan-cyberworx <bhuvan@cyberworx.in>
2018-05-11 19:46:56 +0530

    changes

--- index.php
+++ index.php
@@ -11,6 +11,55 @@
 		</a>
 	</div>
 
+	<div class="main-blocks">
+		<div class="main-box investment-banking">
+			<div class="main-box-title">
+				<h1>Investment Banking</h1>
+			</div>
+			<div class="main-box-description">
+				<p>
+					Launchpad for new startups and businesses.
+				</p>
+			</div>
+		</div>
+
+		<div class="main-box media-entertainment">
+			<div class="main-box-title">
+				<h1>Media & Entertainment</h1>
+			</div>
+			<div class="main-box-description">
+				<p>
+					Get financed for your media startup.
+				</p>
+			</div>
+		</div>
+
+		<div class="main-box about-us">
+			<div class="main-box-title">
+				<h1>About <br> Morris Street</h1>
+			</div>
+			<div class="main-box-description">
+				<p>
+					Get to know in and out of us.
+				</p>
+			</div>
+		</div>
+
+		<div class="main-box commodity-trading">
+			<div class="main-box-title">
+				<h1>Commodity Trading</h1>
+			</div>
+			<div class="main-box-description">
+				<p>
+					Learn how to do commodity trade.
+				</p>
+			</div>
+		</div>
+
+
+
+	</div>
+
 	<div class="press-release-wrapper" id="press-release-wrapper">
 		<div class="press-relase-button" id="press-relase-button">
 			Latest Press Release
